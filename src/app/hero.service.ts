4cfb25a1d865b7a198bca3ea98a7aecc0b42f6c0
Erik Froese <efroese@dius.com.au>
2019-08-15 16:42:06 +1000

    add dashboard component

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,7 +11,7 @@ export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
-  getHeros(): Observable<Hero[]> {
+  getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -17,13 +17,13 @@ export class HeroesComponent implements OnInit {
 
   constructor(private heroService: HeroService) { }
 
-  getHeros(): void {
-    this.heroService.getHeros()
+  getHeroes(): void {
+    this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
   }
 
   ngOnInit() {
-    this.getHeros();
+    this.getHeroes();
   }
 
 }
