83f407c9736c60baf2e3ea3611b71988e2f434bb
Erik Froese <efroese@dius.com.au>
2019-08-15 12:05:19 +1000

    extract hero into its own detail component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,6 +9,8 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
+  selectedHero?: Hero;
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
