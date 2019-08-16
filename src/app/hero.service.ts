8221232d73a45c2527831471307f88cf05b82f94
Erik Froese <efroese@dius.com.au>
2019-08-16 13:57:10 +1000

    fetch data using http from in-memory-api

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,24 +1,31 @@
 import { Injectable } from '@angular/core';
-import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
 import { MessageService } from './message.service';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';  // URL to web api
 
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService, private http: HttpClient) { }
 
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    this.log(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    this.log('HeroService: fetched heroes');
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
 }
